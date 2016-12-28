c55100c7b97800b89449bbbffd90ffb5c3656376
ouyangshenguang <ouyangshenguang@zhixueyun.com>
2016-12-28 10:39:19 +0800

    考试详情页

--- scripts/app/exam/index/index.js
+++ scripts/app/exam/index/index.js
@@ -8,12 +8,27 @@ exports.store = {
     models: {
         exam: {
             url: '../exam/exam'
+        },
+        signup: {
+            url: '../exam/sign-up/detail'
         }
     },
     callbacks: {
         init: function(payload) {
             this.models.exam.set(payload);
             this.get(this.models.exam);
+            this.models.signup.params = { examId: payload.id };
+            this.get(this.models.signup);
+        },
+        signup: function() {
+            var me = this,
+                examId = me.models.exam.data.id;
+            me.models.signup.params = { examId: examId };
+            return me.save(me.models.signup);
+        },
+        revoke: function() {
+            var me = this;
+            return me.del(me.models.signup);
         }
     }
 };
