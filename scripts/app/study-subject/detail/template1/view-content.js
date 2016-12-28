f9999fc6a473fb6065fc8f95070d639747d73c7c
tj <tangjie@netschina.com>
2016-12-28 17:36:36 +0800

    专题前端预览

--- scripts/app/study-subject/detail/template1/view-content.js
+++ scripts/app/study-subject/detail/template1/view-content.js
@@ -2,7 +2,8 @@ exports.type = 'dynamic';
 exports.bindings = {
     layout: true,
     regions: true,
-    subject: false
+    subject: false,
+    state: false
 };
 
 exports.getEntityModuleName = function(key) {
@@ -11,10 +12,12 @@ exports.getEntityModuleName = function(key) {
 
 exports.getEntity = function(key) {
     var region = this.bindings.regions.findByModuleCode(key),
-        subject = this.bindings.subject.data;
+        subject = this.bindings.subject.data,
+        state = this.bindings.state.data;
     return {
         region: region,
-        subject: subject
+        subject: subject,
+        state: state
     };
 };
 
--- scripts/app/study-subject/detail/views/discuss/view-pannel.js
+++ scripts/app/study-subject/detail/views/discuss/view-pannel.js
@@ -1,20 +1,26 @@
 exports.bindings = {
-    subject: false
+    subject: false,
+    state: false
 };
 
 exports.components = [
     function() {
-        var subjectId = this.bindings.subject.data.id;
-        var obj = {
+        var subjectId = this.bindings.subject.data.id,
+            state = this.bindings.state.data || {},
+            available = true;
+        if (state.type === 'preview') {
+            available = false;
+        }
+        return {
             id: 'comment-area',
             name: 'picker',
             options: {
                 picker: 'comment-area',
                 componentId: 'comment-area',
                 businessId: subjectId,
-                businessType: 2
+                businessType: 2,
+                available: available
             }
         };
-        return obj;
     }
 ];
