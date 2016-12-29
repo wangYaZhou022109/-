5b8b6f80c090e3d826ec9fdc5ad6cf1a846a6967
keeleys <keeley.jun@qq.com>
2016-12-29 11:41:15 +0800

    fix audio learntime

--- scripts/app/ext/audio-wavesurfer.js
+++ scripts/app/ext/audio-wavesurfer.js
@@ -1,42 +1,35 @@
 var D = require('drizzlejs'),
     WaveSurfer = require('wavesurfer.js');
-var wavesurfetKit = function(wavesurfet) {
-    return (function(wf) {
-        var beginTime = -1;
-        var learnTime = -1;
-        var init = function() {
-            learnTime = 0;
-        };
-        var begin = function() {
-            beginTime = wf.getCurrentTime();
-        };
-        var end = function() {
-            learnTime += (wf.getCurrentTime() - beginTime);
-            beginTime = wf.getCurrentTime(); // 重置开始时间
-        };
-        var getLearnTime = function() {
-            end();
-            return learnTime;
-        };
-
-        return {
-            init: init,
-            begin: begin,
-            end: end,
-            getLearnTime: getLearnTime,
-        };
-    }(wavesurfet));
+var wavesurfetKit = function() {
+    var beginTime;
+    var learnTime = 0;
+    var audioprocess = function(t) {
+        var minusTime;
+        if (!beginTime) {
+            beginTime = t;
+        }
+        minusTime = Math.abs(t - beginTime);
+        beginTime = t;
+        if (minusTime > 10) return;
+        learnTime += minusTime;
+    };
+    var getLearnTime = function() {
+        return Math.floor(learnTime);
+    };
+    return {
+        audioprocess: audioprocess,
+        getLearnTime: getLearnTime,
+    };
 };
 
 D.ComponentManager.register('audio-waveform', function(view, el, options) {
     var wavesurfer,
-        kit,
+        kit = wavesurfetKit(),
         opt = {
             waveColor: 'red',
             progressColor: 'purple'
         };
-    var events = ['loading', 'pause', 'play', 'ready', 'finish', 'error'];
-
+    var events = ['loading', 'pause', 'play', 'ready', 'finish', 'error', 'audioprocess', 'seek', 'scroll', 'zoom'];
     D.assign(opt, options.opt, { container: el });
 
     wavesurfer = WaveSurfer.create(opt);
@@ -48,14 +41,7 @@ D.ComponentManager.register('audio-waveform', function(view, el, options) {
             }
         });
     }
-
-    kit = wavesurfetKit(wavesurfer);
-    wavesurfer.on('ready', function() {
-        wavesurfer.setVolume(1);
-        kit.init();
-    });
-    wavesurfer.on('play', kit.begin);
-    wavesurfer.on('pause', kit.end);    // 结束播放也会触发暂停事件
+    wavesurfer.on('audioprocess', kit.audioprocess);
     wavesurfer.getLearnTime = kit.getLearnTime;
     return wavesurfer;
 }, function(view, wavesurfer) {
