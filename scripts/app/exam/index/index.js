a0e9dbe9532b2f3839959b69fe6d105ebbcac95d
ouyangshenguang <ouyangshenguang@zhixueyun.com>
2017-01-04 17:44:30 +0800

    相关话题，相关考试，相关人员

--- scripts/app/exam/index/index.js
+++ scripts/app/exam/index/index.js
@@ -9,12 +9,27 @@ exports.store = {
         exam: {
             url: '../exam/exam/exam-signup'
         },
+        relatedExams: {
+            url: '../exam/exam/related-exams'
+        },
+        relatedTopics: {
+            url: '../exam/exam/related-topics'
+        },
+        relatedMembers: {
+            url: '../exam/exam/related-members'
+        },
         down: { url: '../human/file/download' },
     },
     callbacks: {
         init: function(payload) {
             this.models.exam.set(payload);
             this.get(this.models.exam);
+            this.models.relatedExams.params = payload;
+            this.get(this.models.relatedExams);
+            this.models.relatedTopics.params = payload;
+            this.get(this.models.relatedTopics);
+            this.models.relatedMembers.params = payload;
+            this.get(this.models.relatedMembers);
         },
         signUp: function() {
             var exam = this.models.exam;
