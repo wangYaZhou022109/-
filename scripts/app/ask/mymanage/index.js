ced0635c4df35fb0e67789be78633b1e0f2346d2
hxly36 <hxly36@163.com>
2017-02-09 19:18:17 +0800

    审核

--- scripts/app/ask/mymanage/index.js
+++ scripts/app/ask/mymanage/index.js
@@ -1,20 +1,28 @@
 exports.items = {
-    mymanage: 'mymanage'
+    mymanage: 'mymanage',
+    reviewed: 'reviewed'
 };
 
 exports.store = {
     models: {
         params: { data: { isOverdue: '1' } },
-        mymanage: { url: '../ask-bar/my-manage' }
+        mymanage: { url: '../ask-bar/my-manage' },
+        reviewed: { url: '../ask-bar/my-manage/reviewed' }
     },
     callbacks: {
         init: function() {
             var mymanage = this.models.mymanage;
             mymanage.set({ id: 1 });
             return this.get(mymanage);
+        },
+        reviewed: function() {
+            var reviewed = this.models.reviewed;
+            reviewed.set({ id: 1 });
+            return this.get(reviewed);
         }
     }
 };
 exports.afterRender = function() {
     this.dispatch('init');
+    this.dispatch('reviewed');
 };
