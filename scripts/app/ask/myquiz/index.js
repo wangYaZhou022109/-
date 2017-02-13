d7affe19b516ae126f898ec15bd59d42d91ac8c9
luocong <luocong@zhixueyun.com>
2017-02-13 20:03:39 +0800

    我的提问和详情

--- scripts/app/ask/myquiz/index.js
+++ scripts/app/ask/myquiz/index.js
@@ -1,18 +1,27 @@
+var _ = require('lodash/collection');
 exports.items = {
     list: 'list'
 };
 
 exports.store = {
     models: {
-        question: { url: '../ask-bar/myquiz/question' },
-        count: { url: '../ask-bar/myquiz/count' },
+        questions: { url: '../ask-bar/myquiz/question' },
+        question: {
+            url: '../ask-bar/myquiz/question-details',
+            mixin: {
+                getQuestionById: function(data) {
+                    var questions = this.module.store.models.questions.data;
+                    return _.find(questions, ['id', data.id]);
+                }
+            }
+        },
         params: { data: { isOverdue: '1' } }
     },
     callbacks: {
         init: function() {
-            var question = this.models.question;
-            question.set({ id: 1 });
-            return this.get(question);
+            var questions = this.models.questions;
+            questions.set({ id: 1 });
+            return this.get(questions);
         }
     }
 };
--- scripts/app/ask/myquiz/view-list.js
+++ scripts/app/ask/myquiz/view-list.js
@@ -1,9 +1,20 @@
 exports.type = 'dynamic';
 exports.bindings = {
     params: false,
-    question: true,
-    count: false
+    questions: true,
+    question: true
 };
 
+exports.events = {
+    'click details-*': 'details'
+};
+
+exports.handlers = {
+    details: function(data) {
+        var mod = this.module.items['ask-bar/question/details'],
+            question = this.bindings.question.getQuestionById({ id: data });
+        this.app.viewport.ground(mod, { question: question });
+    }
+};
 exports.dataForTemplate = {
 };
