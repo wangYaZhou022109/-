077ab7c1af48f49d8c378737c5ed90705a303347
luocong <luocong@zhixueyun.com>
2017-02-14 17:42:19 +0800

    详情

--- scripts/app/ask/myquiz/index.js
+++ scripts/app/ask/myquiz/index.js
@@ -1,26 +1,22 @@
-var _ = require('lodash/collection');
 exports.items = {
-    list: 'list'
+    list: 'list',
+    detials: ''
 };
 
 exports.store = {
     models: {
         questions: { url: '../ask-bar/myquiz/question' },
-        question: {
-            url: '../ask-bar/myquiz/question-details',
-            mixin: {
-                getQuestionById: function(data) {
-                    var questions = this.module.store.models.questions.data;
-                    return _.find(questions, ['id', data.id]);
-                }
-            }
-        },
         params: { data: { isOverdue: '1' } }
     },
     callbacks: {
         init: function() {
             var questions = this.models.questions;
             questions.set({ id: 1 });
+            return this.get(questions);
+        },
+        remove: function(payload) {
+            var questions = this.models.questions;
+            questions.set(payload);
             return this.get(questions);
         }
     }
--- scripts/app/ask/myquiz/view-list.js
+++ scripts/app/ask/myquiz/view-list.js
@@ -1,20 +1,57 @@
 exports.type = 'dynamic';
 exports.bindings = {
     params: false,
-    questions: true,
-    question: true
+    questions: true
 };
 
 exports.events = {
-    'click details-*': 'details'
+    'click details-*': 'details',
+    'click detailss-*': 'datailss'
 };
 
 exports.handlers = {
     details: function(data) {
-        var mod = this.module.items['ask-bar/question/details'],
+        var mod = this.module.items['ask-bar/myquiz/details'],
             question = this.bindings.question.getQuestionById({ id: data });
         this.app.viewport.ground(mod, { question: question });
+    },
+    datailss: function(data) {
+        var mod = this.module.items['ask-bar/myquiz/details'],
+            question = this.bindings.question.getQuestionById({ id: data });
+        this.app.viewport.ground(mod, { question: question.id });
     }
 };
-exports.dataForTemplate = {
+
+exports.actions = {
+    'click remove-*': 'remove',
+    'click concern-*': 'concern',
+    'click enjoy-*': 'enjoy',
+    'click report-*': 'report'
+};
+
+exports.dataForActions = {
+    remove: function(data) {
+        var me = this;
+        return this.Promise.create(function(resolve) {
+            var message = '确定要删除该数据?';
+            me.app.message.confirm(message, function() {
+                resolve(data);
+            }, function() {
+                resolve(false);
+            });
+        });
+    },
+    concern: function() {
+    },
+    enjoy: function() {
+    },
+    report: function() {
+    }
+};
+
+exports.actionCallbacks = {
+    remove: function() {
+        this.app.message.success('删除成功！');
+        this.module.dispatch('init');
+    }
 };
