85b98c001643816abea1852ee8699772ccbc49c0
luocong <luocong@zhixueyun.com>
2017-02-15 10:10:28 +0800

    我的提问

---
+++ scripts/app/ask/myquiz/details/index.js
@@ -0,0 +1,80 @@
+exports.items = {
+    main: 'main'
+};
+
+exports.store = {
+    models: {
+        details: {
+            url: '../ask-bar/myquiz/question-details'
+        },
+        discuss: { url: '../ask-bar/myquiz/question-discuss' },
+        reply: { url: '../ask-bar/myquiz/question-reply' },
+        state: { data: {} }
+    },
+    callbacks: {
+        refreshrelpy: function() {
+            this.models.state.changed();
+        },
+        init: function(payload) {
+            var question = payload.question;
+            this.models.details.set(question);
+        },
+        questionDetails: function(payload) {
+            var details = this.models.details,
+                data = payload;
+            details.set({ id: data.question.id });
+            return this.get(details);
+        },
+        details: function(payload) {
+            var details = this.models.details;
+            details.set(payload);
+            return this.get(details);
+        },
+        refresh: function(payload) {
+            var details = this.models.details,
+                data = {};
+            data.id = payload[0].id;
+            details.set(data);
+            return this.get(details);
+        },
+        close: function(payload) {
+            var close = this.models.details;
+            close.set(payload);
+            return this.del(close);
+        },
+        boutique: function(payload) {
+            var boutique = this.models.details;
+            boutique.set(payload);
+            return this.save(boutique);
+        },
+        discuss: function(payload) {
+            var discuss = this.models.discuss;
+            discuss.set(payload);
+            return this.save(discuss);
+        },
+        discussdel: function(payload) {
+            var discussdel = this.models.discuss;
+            discussdel.set(payload);
+            return this.del(discussdel);
+        },
+        discusstop: function(payload) {
+            var discusstop = this.models.discuss;
+            var data = payload;
+            data.topsStatus = 1;
+            discusstop.set(data);
+            return this.post(discusstop);
+        },
+        discussboutique: function(payload) {
+            var data = payload;
+            var discussboutique = this.models.discuss;
+            data.essenceStatus = 1;
+            discussboutique.set(data);
+            return this.post(discussboutique);
+        }
+    }
+};
+
+exports.beforeRender = function() {
+    return this.dispatch('questionDetails', this.renderOptions);
+};
+
---
+++ scripts/app/ask/myquiz/details/view-main.js
@@ -0,0 +1,101 @@
+var D = require('drizzlejs');
+exports.type = 'form';
+
+exports.bindings = {
+    state: false,
+    details: true
+};
+exports.events = {
+    'click discuss-answer-*': 'discussanswer'
+};
+
+exports.handlers = {
+    discussanswer: function(id) {
+        var region;
+        var el = this.$('reply-' + id);
+        if (el.style.display === 'none') {
+            el.style.display = 'inline';
+            region = new D.Region(this.app, this.module, el, id);
+            region.show('ask-bar/question/reply', { id: id });
+        } else {
+            el.style.display = 'none';
+        }
+    }
+};
+exports.actions = {
+    'click close': 'close',
+    'click boutique': 'boutique',
+    'click discuss': 'discuss',
+    'click discuss-top-*': 'discusstop',
+    'click discuss-boutique-*': 'discussboutique',
+    'click discuss-del-*': 'discussdel'
+};
+
+// actions绑定的方法调用前要干的事情
+exports.dataForActions = {
+    close: function(payload) {
+        return payload;
+    },
+    boutique: function(payload) {
+        return payload;
+    },
+    discuss: function(payload) {
+        var data = payload;
+        return this.validate() ? data : false;
+    },
+    discussdel: function(payload) {
+        return payload;
+    },
+    discusstop: function(payload) {
+        return payload;
+    },
+    discussboutique: function(payload) {
+        return payload;
+    },
+    replyanswer: function(payload) {
+        return payload;
+    },
+    replydel: function(payload) {
+        return payload;
+    }
+};
+
+// actions绑定的方法调用后要干的事情
+exports.actionCallbacks = {
+    close: function() {
+        this.app.message.success('问题关闭成功！');
+        this.app.viewport.closeModal();
+        this.module.dispatch('refreshquestions');
+    },
+    boutique: function(payload) {
+        this.app.message.success('精品设置成功！');
+        this.module.dispatch('refresh', payload);
+    },
+    discuss: function(payload) {
+        this.app.message.success('讨论发表成功！');
+        this.module.dispatch('refresh', payload);
+    },
+    discussdel: function(payload) {
+        this.app.message.success('讨论删除成功！');
+        this.module.dispatch('refresh', payload);
+    },
+    discusstop: function(payload) {
+        this.app.message.success('置顶成功！');
+        this.module.dispatch('refresh', payload);
+    },
+    discussboutique: function(payload) {
+        this.app.message.success('加精成功！');
+        this.module.dispatch('refresh', payload);
+    }
+};
+
+
+exports.dataForTemplate = {
+    details: function(data) {
+        var obj = data,
+            date = new Date(data.details.createTime);
+        obj.details.createTime = date.getFullYear() + '-' + (date.getMonth() + 1) + '-' + date.getDate()
+        + '   ' + date.getHours() + ':' + date.getMinutes();
+        return obj.details;
+    }
+};
