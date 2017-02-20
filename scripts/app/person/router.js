4352dfd5eb73621c848be445b724d358a153e893
tiancong <tiancong@zhixueyun.com>
2017-02-20 17:40:23 +0800

    响应中心修改

--- scripts/app/person/router.js
+++ scripts/app/person/router.js
@@ -1,15 +1,10 @@
 exports.routes = {
     index: 'showIndex',
-    response: 'showResponse',
     train: 'showTrain'
 };
 
 exports.showIndex = function() {
     return this.app.show('content', 'person/index');
-};
-
-exports.showResponse = function() {
-    return this.app.show('content', 'person/response');
 };
 
 exports.showTrain = function(id) {
--- scripts/app/train/router.js
+++ scripts/app/train/router.js
@@ -1,7 +1,12 @@
 exports.routes = {
-    'index/:id': 'showIndex'
+    'index/:id': 'showIndex',
+    response: 'showResponse'
 };
 
 exports.showIndex = function(id) {
     return this.app.show('content', 'train/index', { id: id });
+};
+
+exports.showResponse = function() {
+    return this.app.show('content', 'train/response');
 };
