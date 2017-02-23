25b59e17ef7ffd9c6b63fcdf2aead31b56063929
wuyulong <wuyulong@zhixueyun.com>
2017-02-23 11:43:10 +0800

    直播详情

--- scripts/app/activity/router.js
+++ scripts/app/activity/router.js
@@ -1,7 +1,12 @@
 exports.routes = {
-    index: 'showIndex'
+    index: 'showIndex',
+    'live-details': 'showLiveDetails'
 };
 
 exports.showIndex = function() {
     return this.app.show('content', 'activity/index');
+};
+
+exports.showLiveDetails = function() {
+    return this.app.show('content', 'activity/live-details');
 };
