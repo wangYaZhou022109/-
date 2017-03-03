04ddf345599392e6a30dec82df22faa54e6583b2
chuanyan <huangchuanyan@zhixueyun.com>
2017-03-03 11:15:15 +0800

    个人中心，我的课程

--- scripts/app/person/course/index.js
+++ scripts/app/person/course/index.js
@@ -1,3 +1,21 @@
 exports.items = {
-
+    top: 'top',
+    menu: 'menu',
+    main: 'main',
+    notice: 'notice'
+};
+exports.store = {
+    models: {
+        state: {}
+    },
+    callbacks: {
+        init: function() {
+            var state = this.models.state;
+            state.data.menu = 'archives';
+            state.data.archives = true;
+        }
+    }
+};
+exports.beforeRender = function() {
+    this.dispatch('init');
 };
