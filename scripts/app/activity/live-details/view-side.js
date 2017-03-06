b653fef5bb3f5fed5a0f75a373b25498773973b5
liutao <liutao@zhixueyun.com>
2017-03-06 11:32:29 +0800

    直播详情页面

---
+++ scripts/app/activity/live-details/view-side.js
@@ -0,0 +1,17 @@
+
+exports.bindings = {
+    gensee: true,
+};
+
+exports.events = {
+};
+
+exports.handlers = {
+};
+
+exports.dataForTemplate = {
+    businesses: function(data) {
+        return data.gensee.businesses;
+    },
+};
+
