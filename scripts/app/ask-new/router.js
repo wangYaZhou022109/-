ef2f5ec43c1438b07ee30497b80253f8971729e1
舒倩 <397314809@qq.com>
2017-03-07 19:56:08 +0800

    添加问吧列表页，话题列表，话题详情，

--- scripts/app/ask-new/router.js
+++ scripts/app/ask-new/router.js
@@ -1,7 +1,23 @@
 exports.routes = {
     index: 'showIndex',
+    management: 'showManagement',
+    'management/list': 'showManagementList',
+    'management/detail': 'showManagementDetail'
+
 };
 
 exports.showIndex = function() {
     return this.app.show('content', 'ask-new/index');
+};
+
+exports.showManagement = function() {
+    return this.app.show('content', 'ask-new/management');
+};
+
+exports.showManagementList = function() {
+    return this.app.show('content', 'ask-new/management/list');
+};
+
+exports.showManagementDetail = function() {
+    return this.app.show('content', 'ask-new/management/detail');
 };
