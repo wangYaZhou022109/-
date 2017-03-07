8867bfc89db6c800aa94c921235a809a5499f450
vivi_wong <1144962942@qq.com>
2017-03-07 18:29:24 +0800

    update answer-paper api

--- scripts/app/exam/exam/answer-paper-2/index.js
+++ scripts/app/exam/exam/answer-paper-2/index.js
@@ -1,6 +1,7 @@
 var $ = require('jquery'),
     _ = require('lodash/collection'),
     D = require('drizzlejs'),
+    E = require('./app/exam/exam-websocket'),
     maps = require('./app/util/maps'),
     qTypes = maps.get('question-types'),
     strings = require('./app/util/strings'),
@@ -10,7 +11,8 @@ var $ = require('jquery'),
         ONE_HUNDRED: 100,
         ZERO: 0,
         ONE: 1,
-        SINGLE_MODE: 1
+        SINGLE_MODE: 1,
+        PC_CLIENT_TYPE: 1
     },
     itemStatus = {
         INIT: 'init',
@@ -27,14 +29,21 @@ var $ = require('jquery'),
         6: 8,
         7: 5,
         8: 4
-    };
+    },
+    submitType = {
+        Auto: 'Auto',
+        Hand: 'Hand'
+    },
+    connect,
+    closeConnect,
+    cancel,
+    timeOutId;
 
 exports.items = {
     side: 'side',
     main: 'main',
     head: 'head',
-    'count-down': 'count-down',
-    tips: ''
+    'count-down': 'count-down'
 };
 
 exports.store = {
@@ -230,9 +239,7 @@ exports.store = {
             mixin: {
                 init: function() {
                     this.load();
-                    if (!this.data) {
-                        this.data = [];
-                    }
+                    if (!this.data) this.data = [];
                 },
                 saveAnswer: function(data) {
                     this.data = _.reject(this.data, ['key', data.key]);
@@ -252,6 +259,39 @@ exports.store = {
                     return _.find(this.data, ['key', questionId]);
                 }
             }
+        },
+        modify: {
+            type: 'localStorage',
+            mixin: {
+                saveAnswer: function(data) {
+                    if (!this.data) this.data = [];
+                    this.data = _.reject(this.data, ['key', data.key]);
+                    this.data.push(data);
+                    this.save();
+                },
+                changeAnswerRecord: function() {
+                    var results = [];
+                    _.forEach(this.data.answers, function(a) {
+                        if (a.type && a.type === 6) {
+                            _.forEach(a.value, function(v) {
+                                results.push(v);
+                            });
+                        } else {
+                            results.push(a);
+                        }
+                    });
+                    return JSON.stringify(_.map(results, function(a) {
+                        return {
+                            questionId: a.key,
+                            answer: _.map(a.value, 'value').join(',')
+                        };
+                    }));
+                }
+
+            }
+        },
+        form: {
+            url: '../exam/exam-record/submitPaper'
         }
     },
     callbacks: {
@@ -306,6 +346,39 @@ exports.store = {
         waitingCheck: function(payload) {
             this.models.mark.waitingCheck(payload.questionId);
             this.app.message.success(strings.get('operation-success'));
+        },
+        submitPaper: function(payload) {
+            var me = this,
+                examRecordId = this.module.store.models.exam.data.examRecord.id,
+                data = {
+                    examRecordId: examRecordId,
+                    submitType: payload.submitType,
+                    answerRecords: this.models.modify.changeAnswerRecord(),
+                    clientType: constant.PC_CLIENT_TYPE
+                };
+
+            if (payload.submitType !== submitType.Auto) cancel();
+
+            this.models.form.set(data);
+            return this.post(this.models.form).then(function() {
+                if (payload.submitType === submitType.Auto) {
+                    this.app.message.success('答案已自动保存成功');
+                    me.models.modify.clear();
+                } else {
+                    me.models.exam.clear();
+                    me.models.state.clear();
+                    me.models.types.clear();
+                    me.models.mark.clear();
+                    me.models.answer.clear();
+                    me.models.modify.clear();
+                    this.app.message.success('交卷成功');
+                    setTimeout(window.close, 500);
+                }
+            });
+        },
+        delay: function(payload) {
+            this.models.countDown.data.delay = payload.delay;
+            this.models.countDown.changed();
         }
     }
 };
@@ -317,6 +390,49 @@ exports.beforeRender = function() {
 
 
 exports.afterRender = function() {
+    var me = this,
+        examId = this.store.models.exam.data.id,
+        getRandom = function() {
+            var r = Math.random() * 1,
+                min = Number(r.toFixed(2)),
+                ms = (min + 1) * (1000 * 60);
+            return ms;
+        },
+        random = getRandom(),
+        autoSubmit = function() {
+            return me.dispatch('submitPaper', { submitType: submitType.Auto }).then(function() {
+                timeOutId = setTimeout(autoSubmit, random);
+            });
+        };
+
+    autoSubmit();
+
+    timeOutId = setTimeout(autoSubmit, random);
+
+    connect(examId, function() {
+        me.app.message.error('你本次考试已被管理员强制交卷');
+        return me.dispatch('submit', { submitType: submitType.Hand }).then(function() {
+            closeConnect();
+        });
+    }, function(delay) {
+        return me.dispatch('delay', { delay: Number(delay) });
+    });
+};
+
+connect = function(examId, submitPaper, timeExpand) {
+    E.connect(examId, {
+        submitPaper: submitPaper,
+        timeExpand: timeExpand
+    });
+};
+
+closeConnect = function() {
+    E.disconnect();
+};
+
+cancel = function() {
+    clearTimeout(timeOutId);
+    timeOutId = undefined;
 };
 
 changeToFullScreen = function() {
