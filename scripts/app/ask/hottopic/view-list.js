2ef3329881048d8762c542fe6999269ac5857ddd
张华 <zhanghua@zhixueyun.com>
2017-03-10 10:44:41 +0800

    Signed-off-by: 张华 <zhanghua@zhixueyun.com> 业务修正

--- scripts/app/ask/hottopic/view-list.js
+++ scripts/app/ask/hottopic/view-list.js
@@ -1,13 +1,19 @@
 
+var D = require('drizzlejs');
+var $ = require('jquery');
 exports.type = 'dynamic';
 exports.bindings = {
     topic: true
 };
 
 exports.events = {
+    'click topic-more': 'topicMore',
 };
 
 exports.handlers = {
+    topicMore: function() {
+        this.app.show('content', 'ask/topic');
+    }
 };
 
 exports.dataForTemplate = {
