55f0a589c60939b182971eb5d4e44a64395e02c6
hanxiaoyu <hanxiaoyu@zhixueyun.com>
2017-03-10 16:58:12 +0800

    个人中心我的档案

--- scripts/app/center/archives/certificate/view-main.js
+++ scripts/app/center/archives/certificate/view-main.js
@@ -15,23 +15,6 @@ exports.dataForTemplate = {
         _.map(data.list || [], function(role, i) {
             var r = role;
             r.i = i + 1 + ((pageNum - 1) * 10);
-            if (!r.integralScore) {
-                r.integralScore = '0';
-            }
-            if (!r.downloadMemberCount) {
-                r.downloadMemberCount = '0';
-            }
-            if (!r.browseCount) {
-                r.browseCount = '0';
-            }
-            if (!r.browseMemberCount) {
-                r.browseMemberCount = '0';
-            }
-            if (r.type === '0' || r.type === '1') {
-                r.type = '音／视频';
-            } else {
-                r.type = '文档';
-            }
         });
         return data.list;
     },
