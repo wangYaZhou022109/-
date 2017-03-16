8fc751b4600fbf7122804c020d486b4de7adb690
ouyangshenguang <ouyangshenguang@zhixueyun.com>
2017-03-16 18:13:08 +0800

    我的考试

--- scripts/app/center/exam/index.js
+++ scripts/app/center/exam/index.js
@@ -1,3 +1,35 @@
+var D = require('drizzlejs');
 exports.items = {
-    main: 'main'
+    main: 'main',
+    filter: 'filter'
+};
+
+exports.store = {
+    models: {
+        exams: {
+            url: '../exam/exam/details',
+            type: 'pageable',
+            root: 'items'
+        },
+        search: { data: { startTimeOrderBy: 0 } }
+    },
+    callbacks: {
+        init: function() {
+            var searchModel = this.models.search,
+                exams = this.models.exams;
+            D.assign(exams.params, searchModel.data);
+            return this.get(exams);
+        },
+        search: function(params) {
+            var searchModel = this.models.search,
+                exams = this.models.exams;
+            D.assign(exams.params, D.assign(searchModel.data, params));
+            this.get(exams);
+            searchModel.changed();
+        }
+    }
+};
+
+exports.beforeRender = function() {
+    this.dispatch('init', this.renderOptions);
 };
