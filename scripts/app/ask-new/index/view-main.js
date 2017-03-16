e13e052c8c914b53dbbbd009ae6fd3ac5f5cdd3e
test <c1575622409@163.com>
2017-03-16 17:18:36 +0800

    问吧：与我相关页面新增

--- scripts/app/ask-new/index/view-main.js
+++ scripts/app/ask-new/index/view-main.js
@@ -1,12 +1,44 @@
 var $ = require('jquery');
 
 exports.events = {
-    'click item-*': 'toggleItem'
+    'click item-*': 'toggleItem',
+    'click button-*': 'togglePage',
+    'click icon-*': 'toggleIcon'
 };
 
 exports.handlers = {
     toggleItem: function(id) {
         $(this.$('item-' + id)).addClass('active').siblings().removeClass('active');
         $(this.$('tabs-cont-item-' + id)).addClass('active').siblings().removeClass('active');
+    },
+    togglePage: function(id) {
+        var div1;
+        var div2;
+        var div3;
+        var dv;
+        div1 = $(this.$('button-1')).parent('div').parent('div');
+        div2 = $(this.$('button-2')).parent('div').parent('div');
+        dv = $(this.$('button-3')).parent('div').parent('div').parent('div');
+        div3 = $(dv.children('div')[2]);
+        if (id === '1') {
+            div1.hide();
+            div2.show();
+            div3.hide();
+        }
+        if (id === '2') {
+            div1.show();
+            div2.hide();
+            div3.hide();
+        }
+        if (id === '3') {
+            div1.hide();
+            div2.hide();
+            div3.show();
+        }
+    },
+    toggleIcon: function(id) {
+        var that;
+        that = $(this.$('icon-' + id));
+        that.toggleClass('icon-opacity0');
     }
 };
