751a69dcc31fc29ba0d7589aa9d86cf9401b1b16
hanxiaoyu <hanxiaoyu@zhixueyun.com>
2017-03-18 17:13:46 +0800

    个人中心消息

--- scripts/app/home/message/detail/index.js
+++ scripts/app/home/message/detail/index.js
@@ -1,3 +1,20 @@
 exports.items = {
     main: 'main'
 };
+
+exports.store = {
+    models: {
+        message: { url: '../system/message' }
+    },
+    callbacks: {
+        init: function(paylaod) {
+            var message = this.models.message;
+            message.set({ id: paylaod.id });
+            return this.get(message);
+        }
+    }
+};
+
+exports.beforeRender = function() {
+    this.dispatch('init', this.renderOptions);
+};
