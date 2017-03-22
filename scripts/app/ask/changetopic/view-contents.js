eb4f5efc3952e7789a86c51f2bb6c12446a90c50
张华 <zhanghua@zhixueyun.com>
2017-03-22 20:21:16 +0800

    Signed-off-by: 张华 <zhanghua@zhixueyun.com> 业务修正

--- scripts/app/ask/changetopic/view-contents.js
+++ scripts/app/ask/changetopic/view-contents.js
@@ -1,5 +1,4 @@
-var D = require('drizzlejs');
-var $ = require('jquery');
+
 exports.bindings = {
     state: true,
     topicname: true,
@@ -7,20 +6,13 @@ exports.bindings = {
 };
 
 exports.events = {
-    'click apply-topic': 'showApplyTopic',
-    'click detail-*': 'toggleMore'
+    'click apply-topic': 'showApplyTopic'
 };
 
 exports.handlers = {
     showApplyTopic: function() {
         var model = this.module.items['ask/topicsquare/apply-topic'];
         this.app.viewport.modal(model);
-    },
-    toggleMore: function(id, e, target) {
-        var region;
-        var el = $(target).parents('.activity-category')[0];
-        region = new D.Region(this.app, this.module, el, id);
-        region.show('ask/topicsquare/topicdetail', { id: id });
     }
 };
 exports.actions = {
