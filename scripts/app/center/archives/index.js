add321253113a86994462f551ccb61510b85b853
hanxiaoyu <hanxiaoyu@zhixueyun.com>
2017-03-22 21:12:33 +0800

    个人中心

--- scripts/app/center/archives/index.js
+++ scripts/app/center/archives/index.js
@@ -11,16 +11,16 @@ exports.store = {
                 menuId: '0'
             }
         },
-        detailMenu: { data: [
+        detailMenu: { data: [ // 菜单id一定要按索引顺序
             { id: '0', name: '课程', url: 'course' },
             { id: '1', name: '专题', url: 'course' },
-            { id: '2', name: '班级', url: 'class' },
-            { id: '3', name: '直播', url: 'live' },
-            { id: '4', name: '考试', url: 'exam' },
-            { id: '5', name: '调研', url: 'research' },
-            { id: '6', name: '知识', url: 'knowledge' },
-            { id: '7', name: '问吧', url: 'ask' },
-            { id: '8', name: '证书', url: 'certificate' }
+            // { id: '2', name: '班级', url: 'class' },
+            { id: '2', name: '直播', url: 'live' },
+            { id: '3', name: '考试', url: 'exam' },
+            { id: '4', name: '调研', url: 'research' },
+            { id: '5', name: '知识', url: 'knowledge' },
+            { id: '6', name: '问吧', url: 'ask' },
+            { id: '7', name: '证书', url: 'certificate' }
         ] },
         statistics: { url: '../course-study/course-study-progress/statistics', autoLoad: 'after' }
     },
