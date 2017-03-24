94350a68370597a512f7a03b06e708de48710f3a
张华 <zhanghua@zhixueyun.com>
2017-03-24 09:31:00 +0800

    Signed-off-by: 张华 <zhanghua@zhixueyun.com> 业务修正

--- scripts/app/ask/expertapply/view-details.js
+++ scripts/app/ask/expertapply/view-details.js
@@ -1,23 +1,32 @@
 
 exports.bindings = {
+    innerExpert: true,
+    topicname: true,
+    state: true
 };
 
 exports.actions = {
-    'click release': 'release'
+    'click add-topic-*': 'addTopic',
+    'click del-topic-*': 'delTopic'
 };
+// exports.dataForActions = {
+// };
 
-exports.dataForActions = {
-    release: function(payload) {
-        return payload;
-    }
-};
+// exports.actionCallbacks = {
+// };
 
-exports.actionCallbacks = {
-    release: function() {
-        this.app.message.success('操作成功！');
-        setTimeout(function() {
-            window.location.reload();
-        }, 1000);
-    }
-};
+exports.components = [function() {
+    var data = {},
+        innerExpert = this.bindings.innerExpert.data;
+    if (innerExpert.member) data.value = innerExpert.member.headPortrait;
+    return {
+        id: 'headFile',
+        name: 'picker',
+        options: {
+            picker: 'upload',
+            inputName: 'headPortrait',
+            data: data
+        }
+    };
+}];
 
