1e77b053d19166a4568a79f87040acf15ce6c45c
hanxiaoyu <hanxiaoyu@zhixueyun.com>
2017-03-24 17:55:52 +0800

    个人中心

--- scripts/app/center/edit/index.js
+++ scripts/app/center/edit/index.js
@@ -1,22 +1,78 @@
-var $ = require('jquery');
+var D = require('drizzlejs');
 
 exports.items = {
-    'base-info': 'base-info',
-    security: 'security'
+    main: 'main',
+    'center/edit/info': { isModule: true }
 };
 
-exports.buttons = [{
-    text: '保存'
-}];
 
-
-exports.events = {
-    'click item-*': 'toggleItem'
-};
-
-exports.handlers = {
-    toggleItem: function(id) {
-        $(this.$('item-' + id)).addClass('active').siblings().removeClass('active');
-        $(this.$('tabs-cont-item-' + id)).addClass('active').siblings().removeClass('active');
+exports.store = {
+    models: {
+        member: { url: '../human/member/edit-detail', autoLoad: 'after' },
+        memberForm: { url: '../human/member/update-person' },
+        menus: { data: [
+                { id: '0', name: '基本信息', url: 'info', current: true },
+                { id: '1', name: '安全设置', url: 'security' },
+        ] },
+        state: {
+            data: {
+                menu: 'info', // 初始菜单
+            }
+        },
+    },
+    callbacks: {
+        init: function() {
+            this.models.memberForm.clear();
+        },
+        save: function(options) {
+            var memberForm = this.models.memberForm,
+                me = this;
+            D.assign(memberForm.data, options);// 把新值和旧值合并,用新值覆盖旧值
+            return this.put(memberForm).then(function(data) {
+                if (data[0].password === '2') {
+                    me.app.message.error('密码修改失败，旧密码输入不正确');
+                } else {
+                    me.app.message.success('修改成功');
+                }
+            });
+        }
     }
 };
+
+exports.afterRender = function() {
+    this.dispatch('init');
+};
+
+exports.buttons = [
+    { text: '保存',
+        fn: function(data) {
+            var eg = /^(?![0-9]*$)[a-zA-Z0-9]{6,20}$/;
+            var emg = /^([a-zA-Z0-9_-])+@([a-zA-Z0-9_-])+(.[a-zA-Z0-9_-])+/;
+            if (data.summary && data.summary.length > 200) {
+                this.app.message.error('个人简介字数不能大于200');
+                return false;
+            }
+            if (data.password) {
+                if (!data.oldPassword) {
+                    this.app.message.error('请输入旧密码');
+                    return false;
+                }
+                if (!eg.test(data.password)) {
+                    this.app.message.error('密码由6-20位数字、字母或符号组合');
+                    return false;
+                }
+                if (data.rePassword !== data.password) {
+                    this.app.message.error('确认密码与新密码不一致');
+                    return false;
+                }
+            }
+            if (data.newEmail) {
+                if (!emg.test(data.newEmail)) {
+                    this.app.message.error('邮箱输入不正确');
+                    return false;
+                }
+            }
+            return this.dispatch('save', data);
+        }
+    }
+];
