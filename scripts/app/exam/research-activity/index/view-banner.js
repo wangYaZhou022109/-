00686c159ba7f9fc9ff0222f1009b0858dbd9e07
舒倩 <397314809@qq.com>
2017-03-29 18:47:32 +0800

    调整考试调研、详情等，添加postcss sourcemap,添加分享组件，优化其它细节，

--- scripts/app/exam/research-activity/index/view-banner.js
+++ scripts/app/exam/research-activity/index/view-banner.js
@@ -1,3 +1,16 @@
 exports.bindings = {
     research: true
 };
+
+exports.events = {
+    'click do-research': 'doResearch'
+};
+
+exports.handlers = {
+    doResearch: function() {
+        var me = this;
+        return this.module.dispatch('getRecordByResearch').then(function() {
+            me.app.viewport.modal(me.module.items['research-tips']);
+        });
+    }
+};
