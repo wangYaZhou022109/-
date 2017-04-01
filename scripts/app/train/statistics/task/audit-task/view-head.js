d769f0b98481ee3d8305d1d0f2fbfaa9abed69dd
liushunan <liushunan@zhixueyun.com>
2017-04-01 17:44:02 +0800

    作业审核更新

--- scripts/app/train/statistics/task/audit-task/view-head.js
+++ scripts/app/train/statistics/task/audit-task/view-head.js
@@ -1,5 +1,8 @@
+var $ = require('jquery');
+
 exports.bindings = {
     all: true,
+    state: true,
 };
 
 exports.events = {
@@ -10,4 +13,32 @@ exports.handlers = {
     closeTask: function() {
         window.close();
     },
+};
+
+exports.actions = {
+    'click approval': 'approval'
+};
+
+exports.dataForActions = {
+    approval: function() {
+        // var state = this.bindings.state.data,
+        //     taskMemberId = state.id;
+        // return {
+        //     taskMemberId: taskMemberId,
+        //     score: $(this.$('score')).val(),
+        //     comment: $(this.$('comment')).val(),
+        //     state: $(this.$$('[name="state"]:checked')).val(),
+        // };
+        var taskMemberId = this.bindings.state.data.id,
+            model = this.module.items.side,
+            score = $(model.$('score')).val(),
+            comment = $(model.$('comment')).val(),
+            state = $(model.$$('[name="state"]:checked')).val();
+        return {
+            taskMemberId: taskMemberId,
+            score: score,
+            comment: comment,
+            state: state,
+        };
+    }
 };
