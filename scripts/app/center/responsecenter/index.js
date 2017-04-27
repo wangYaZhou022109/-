122af0d91c24dbc0c09a13f31465700d713a8972
xiaoqing <c1575622409@163.com>
2017-04-27 10:09:10 +0800

    问吧  回复下拉

---
+++ scripts/app/center/responsecenter/index.js
@@ -0,0 +1,5 @@
+exports.items = {
+    'responsecenter-title': 'responsecenter-title',
+    'responsecenter-table': 'responsecenter-table',
+    'responsecenter-footer': 'responsecenter-footer'
+};
---
+++ scripts/app/center/responsecenter/student/index.js
@@ -0,0 +1,6 @@
+exports.items = {
+    student: 'student',
+    'center/responsecenter/student': { isModule: true }
+};
+
+exports.title = '班级成员';
