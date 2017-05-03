0559d9dd6ecb6080e8bb771b24bff56a497a6a8e
vivi_wong <1144962942@qq.com>
2017-05-03 20:12:01 +0800

    修改BUG

---
+++ scripts/app/exam/exam/index/view-main.js
@@ -0,0 +1,8 @@
+exports.bindings = {
+    state: true
+};
+
+exports.afterRender = function() {
+    var mod = this.module.items['activity/index/exam-prompt'];
+    this.app.viewport.modal(mod, { examId: this.bindings.state.data.examId });
+};
--- scripts/app/exam/strings.js
+++ scripts/app/exam/strings.js
@@ -11,7 +11,7 @@ module.exports = {
     'exam.answer-paper.force-submit': '你本次考试已被管理员强制交卷',
     'exam.answer-paper.submit-success': '交卷成功，评卷老师将会尽快为你评卷',
     'exam.answer-paper.switch-screen.full-times': '切屏次数已满，强制交卷',
-    'exam.answer-paper.switch-screen.remian-times': '还剩余{0}次切屏',
+    'exam.answer-paper.switch-screen.remain-times': '还剩余{0}次切屏',
     'exam.answer-paper.time-out.submit': '交卷时间到,你本次考试已被强制交卷',
     'exam.answer-paper.remain-one-mins': '离考试结束不到1分钟，考试时间结束后系统将自动为你提交答案',
     'exam.answer-paper.remain-five-mins': '你好，离考试结束还有5分钟'
