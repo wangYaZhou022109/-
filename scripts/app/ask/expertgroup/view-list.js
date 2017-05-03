8b5934a16e1965815ab759befddbcaea05476622
xiaoqing <maxiaoqing@zhixueyun.com>
2017-05-03 10:00:01 +0800

    merge xq

--- scripts/app/ask/expertgroup/view-list.js
+++ scripts/app/ask/expertgroup/view-list.js
@@ -9,7 +9,7 @@ exports.bindings = {
 
 exports.events = {
     'click activate*': 'activate',
-    'click myself*': 'myself',
+    // 'click myself*': 'myself',
     'click apply*': 'apply',
     'click expert-*': 'details'
 };
@@ -19,9 +19,9 @@ exports.handlers = {
         var model = this.module.items['ask/expertactivation'];
         this.app.viewport.modal(model);
     },
-    myself: function() {
-        this.app.show('content', 'ask/iamexpert');
-    },
+    // myself: function() {
+    //     this.app.show('content', 'ask/iamexpert');
+    // },
     apply: function() {
         var model = this.module.items['ask/applyexpertaptitude'];
         this.app.viewport.modal(model, { bindings: this.bindings });
@@ -54,6 +54,22 @@ exports.dataForActions = {
 };
 
 exports.actionCallbacks = {
+    follow: function(data) {
+        var concern = data[0];
+        var unf = this.$('unfollow-expert-' + concern.concernId);
+        var f = this.$('follow-expert-' + concern.concernId);
+        this.app.message.success('关注成功');
+        f.hidden = true;
+        unf.hidden = false;
+    },
+    unfollow: function(data) {
+        var concern = data[0];
+        var unf = this.$('unfollow-expert-' + concern.concernId);
+        var f = this.$('follow-expert-' + concern.concernId);
+        this.app.message.success('取消成功');
+        f.hidden = false;
+        unf.hidden = true;
+    }
 };
 
 exports.dataForTemplate = {
@@ -65,7 +81,11 @@ exports.dataForTemplate = {
                 url = {};
             if (typeof obj.member !== 'undefined') {
                 url = obj.member.headPortrait;
-                obj.member.headPortrait = me.bindings.down.getFullUrl() + '?id=' + url;
+                if (typeof url === 'undefined' || url === null || url === '') {
+                    obj.member.headPortrait = 'images/default-userpic.png';
+                } else {
+                    obj.member.headPortrait = me.bindings.down.getFullUrl() + '?id=' + url;
+                }
             }
         });
         return expertlist;
