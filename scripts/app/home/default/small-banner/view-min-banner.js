a5c0f9e816ff78122a58e67d86df4989a352ea68
chengzhi <chengzhi@zhixueyun.com>
2017-05-04 17:11:14 +0800

    广告预览

--- scripts/app/home/default/small-banner/view-min-banner.js
+++ scripts/app/home/default/small-banner/view-min-banner.js
@@ -1,3 +1,4 @@
+var _ = require('lodash/collection');
 exports.bindings = {
     smallBanners: true
 };
@@ -9,3 +10,16 @@ exports.components = [{
         slider: true
     }
 }];
+
+exports.dataForTemplate = {
+    smallBanners: function(data) {
+        var smallBanners = data.smallBanners || [];
+        _.map(smallBanners, function(item) {
+            var r = item;
+            if (r.linkType === 5) {
+                r.linkAddress += r.id;
+            }
+        });
+        return smallBanners;
+    }
+};
