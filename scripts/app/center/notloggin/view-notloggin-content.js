eab101d58e9f5b4cb2917343dc05a020963a1dff
xiaoqing <maxiaoqing@zhixueyun.com>
2017-05-05 10:43:12 +0800

    未登录人员信息

--- scripts/app/center/notloggin/view-notloggin-content.js
+++ scripts/app/center/notloggin/view-notloggin-content.js
@@ -1,2 +1,13 @@
+var $ = require('jquery');
 
+exports.events = {
+    'click next': 'toggleView'
+};
+
+exports.handlers = {
+    toggleView: function() {
+        $(this.$('notloggin')).addClass('hide');
+        $(this.$('loggin')).addClass('show').removeClass('hide');
+    }
+};
 
