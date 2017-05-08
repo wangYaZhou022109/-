38c83cc889a573610be4c17a3c684004ec45ee4a
tangjie <tangjie@zhixueyun.com>
2017-05-08 19:47:43 +0800

    bug-fixed

--- scripts/app/study/subject/detail/views/content/index.js
+++ scripts/app/study/subject/detail/views/content/index.js
@@ -1,7 +1,6 @@
 var _ = require('lodash/collection');
 exports.items = {
     pannel: 'pannel',
-    // 'exam-tips': '',
     'research-tips': '',
     'activity/index/exam-prompt': { isModule: true }
 };
@@ -47,55 +46,35 @@ exports.store = {
         },
         examStatus: { url: '../exam/exam/status' },
         researchStatus: { url: '../exam/research-activity/status' },
-        exams: { url: '../exam/exam/basic-by-ids' }
+        exams: { url: '../exam/exam/basic-by-ids' },
+        gensees: { url: '../course-study/gensee/front/find-by-ids' }
     },
     callbacks: {
         init: function(options) {
             var subject = options.subject,
-                studyProgress = subject.studyProgress,
-                courseChapters = subject.courseChapters,
-                courseChapterSections = courseChapters[0].courseChapterSections,
                 exams = this.models.exams,
-                examIds,
+                gensees = this.models.gensees,
                 me = this;
-            if (studyProgress && (!studyProgress.currentChapterId || !studyProgress.currentSectionId)) {
-                studyProgress.currentChapterId = courseChapters[0].id;
-                studyProgress.currentSectionId = courseChapterSections[0].id;
-                subject.studyProgress = studyProgress;
-            }
             this.models.region.set(options.region);
             this.models.subject.set(subject);
             this.models.state.set(options.state || {});
-            // this.chain([(function() {
-            //     var researchIdArr = _.map(me.models.subject.findSectionsForType(12), 'resourceId') || [];
-            //     var researchQueIdArr = _.map(me.models.subject.findSectionsForType(13), 'resourceId') || [];
-            //     var researchIds = researchIdArr.concat(researchQueIdArr).join();
-            //     if (researchIds) {
-            //         me.models.researchStatus.params = { researchIds: researchIds };
-            //         me.get(me.models.researchStatus).then(function(data) {
-            //             subject.courseChapters = initProgress(subject.courseChapters, 12, data[0]);
-            //             me.models.subject.set(subject);
-            //             me.models.subject.changed();
-            //         });
-            //     }
-            // }()), (function() {
-            //     var examIds = _.map(me.models.subject.findSectionsForType(9), 'resourceId').join();
-            //     if (examIds) {
-            //         me.models.examStatus.params = { examIds: examIds };
-            //         me.get(me.models.examStatus).then(function(data) {
-            //             subject.courseChapters = initProgress(subject.courseChapters, 9, data[0]);
-            //             me.models.subject.set(subject);
-            //             me.models.subject.changed();
-            //         });
-            //     }
-            // }())], true);
-            examIds = _.map(this.models.subject.findSectionsForType(9), 'resourceId').join();
-            if (examIds) {
-                exams.params = { ids: examIds };
-                me.get(exams).then(function(data) {
-                    me.models.subject.setSectionResource(data[0]);
-                });
-            }
+            this.chain([(function() {
+                var examIds = _.map(me.models.subject.findSectionsForType(9), 'resourceId').join();
+                if (examIds) {
+                    exams.params = { ids: examIds };
+                    me.get(exams).then(function(data) {
+                        me.models.subject.setSectionResource(data[0]);
+                    });
+                }
+            }()), (function() {
+                var genseeIds = _.map(me.models.subject.findSectionsForType(14), 'resourceId').join();
+                if (genseeIds) {
+                    gensees.params = { ids: genseeIds };
+                    me.get(gensees).then(function(data) {
+                        me.models.subject.setSectionResource(data[0]);
+                    });
+                }
+            }())], true);
         },
         updateProgress: function(payload) {
             var model = this.models.updateProgress;
@@ -113,28 +92,3 @@ exports.store = {
 exports.beforeRender = function() {
     this.dispatch('init', this.renderOptions);
 };
-
-// initProgress = function(courseChapters, sectionType, businessProgress) {
-//     _.map(courseChapters, function(chapter) {
-//         _.map(chapter.courseChapterSections, function(section) {
-//             var sec = section,
-//                 status;
-//             if (sec.sectionType === sectionType) {
-//                 if (sectionType === 9) {
-//                     status = _.find(businessProgress, { examId: sec.resourceId });
-//                     if (status && status.status === 2) {
-//                         sec.progress = D.assign(sec.progress || {}, { finishStatus: 1 });
-//                     }
-//                 } else if (sec.sectionType === 12) {
-//                     status = _.find(businessProgress, { researchQuestionaryId: sec.resourceId });
-//                     if (status && status.status === 1) {
-//                         sec.progress = D.assign(sec.progress || {}, { finishStatus: 2 });
-//                     }
-//                 }
-//             }
-//             return sec;
-//         });
-//         return chapter.courseChapterSections;
-//     });
-//     return courseChapters;
-// };
