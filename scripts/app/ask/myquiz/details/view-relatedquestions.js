5b6f5c30350a6d1da13d17951b9b3161e89bd7c7
hxly036 <hanxiaoming@zhixueyun.com>
2017-05-16 19:57:59 +0800

    代码提交

--- scripts/app/ask/myquiz/details/view-relatedquestions.js
+++ scripts/app/ask/myquiz/details/view-relatedquestions.js
@@ -1,30 +1,4 @@
 
-exports.type = 'dynamic';
 exports.bindings = {
-    state: false,
-    details: true,
-    question: true,
-};
-exports.events = {
-};
-
-exports.handlers = {
-};
-
-
-exports.bindings = {
-    relatedquestions: true
-};
-
-exports.getEntityModuleName = function(key) {
-    return 'ask/' + key;
-};
-exports.getEntity = function() {
-    return {
-        state: this.bindings.relatedquestions.data
-    };
-};
-
-exports.dataForEntityModule = function(entity) {
-    return entity;
+    question: true
 };
