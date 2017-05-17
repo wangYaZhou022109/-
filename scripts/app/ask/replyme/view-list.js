7e2e58c642098c8cd38c9a18318a8642e91ce06f
liushunan <liushunan@zhixueyun.com>
2017-05-17 09:06:24 +0800

    我的回复/回复我的

--- scripts/app/ask/replyme/view-list.js
+++ scripts/app/ask/replyme/view-list.js
@@ -1,14 +1,17 @@
 var D = require('drizzlejs');
+var _ = require('lodash/collection');
 var $ = require('jquery');
 exports.type = 'dynamic';
 exports.bindings = {
     params: false,
+    page: true,
     replyme: true
 };
 
 exports.events = {
     'click myreply-details-*': 'showDetails',
-    'click report-*': 'report'
+    'click report-*': 'report',
+    'click shareTo-*': 'shareTo',
 };
 
 exports.handlers = {
@@ -37,6 +40,75 @@ exports.handlers = {
         if (id.indexOf('_') !== -1) {
             data = id.split('_');
             this.app.viewport.modal(this.module.items['ask/report'], { id: data[1], objectType: data[0] });
+        }
+    },
+    shareTo: function(data) {
+        var value = data.split('_');
+        var page = this.bindings.page;
+        var subject = page.findById(value[1]);
+        var templateContent = '',
+            templateCode = value[0],
+            webUrl = window.location.protocol + '//' + window.location.host, // 域名加端口
+            id = subject.id, // 分享对象id
+            type = subject.shareType, // 分享类型
+            pics = subject.thumbnail || 'images/default-cover/default_spceial.jpg', // 图片，多图传数组 ['','']
+            title = subject.title, // 分享内容的标题
+            typeName = '',
+            fullUrl = '',
+            pic = '';// 图片地址
+        if (subject) {
+            if (type === '1') {
+                typeName = '【问题】';
+            } else if (type === '2') {
+                typeName = '【文章】';
+            } else if (type === '3') {
+                typeName = '【讨论】';
+            }
+
+            if (pics) {
+                if (pics instanceof Array && pics.length > 0) {
+                    _.forEach(pics, function(p) {
+                        if (p.indexOf('default_') > 0) {
+                            pic += webUrl + '/' + p;
+                        } else {
+                            pic += webUrl + '/api/v1/human/file/download?id=' + p;
+                        }
+                        pic += '|';
+                    });
+                    if (pics.length > 0) {
+                        pic = pic.substring(0, pic.length - 1);
+                    }
+                } else if (pics.indexOf('default_') > 0) {
+                    pic += webUrl + '/' + pics;
+                } else {
+                    pic += webUrl + '/api/v1/human/file/download?id=' + pics;
+                }
+            }
+            templateContent = subject.content;
+            if (templateCode === 'qzone') { // QQ空间分享
+                fullUrl = 'http://sns.qzone.qq.com/cgi-bin/qzshare/cgi_qzshare_onekey?';
+                fullUrl += 'title=' + typeName + ' ' + title;
+                fullUrl += '&desc=' + templateContent;
+                fullUrl += '&url=' + encodeURIComponent(webUrl + '/#/share/' + id + '/' + type);
+                if (pic) {
+                    fullUrl += '&pics=' + pic;
+                }
+                window.open(fullUrl, '_blank');
+            } else if (templateCode === 'weibo') { // 新浪微博分享
+                fullUrl = 'http://v.t.sina.com.cn/share/share.php?';
+                fullUrl += 'title=' + templateContent + ' ' + typeName + ' ' + title;
+                fullUrl += '&url=' + encodeURIComponent(webUrl + '/#/share/' + id + '/' + type);
+                if (pic) {
+                    fullUrl += '&pic=' + pic;
+                }
+                fullUrl += '&appkey=3697029777';
+                window.open(fullUrl, '_blank');
+            } else if (templateCode === 'bar') { // 分享到问吧
+                // me.app.viewport.modal(me.module.items.bar, {
+                //    shareObjId: id,
+                //    shareType: type
+                // });
+            }
         }
     }
 };
@@ -75,3 +147,20 @@ exports.actionCallbacks = {
     }
 };
 
+exports.dataForTemplate = {
+    page: function(data) {
+        var page = data.page;
+        var me = this;
+        // var flag = true;
+        _.forEach(page, function(value) {
+            var obj = value,
+                url = obj.member.headPortrait;
+            if (typeof url === 'undefined' || url === null || url === '') {
+                obj.member.headPortrait = 'images/default-userpic.png';
+            } else {
+                obj.member.headPortrait = me.bindings.down.getFullUrl() + '?id=' + url;
+            }
+        });
+        return page;
+    },
+};
