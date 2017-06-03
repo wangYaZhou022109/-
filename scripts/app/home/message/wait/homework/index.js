4af0ed3e727a57b8cd85e1aa3a28fe875266441e
tangjie <tangjie@zhixueyun.com>
2017-06-03 17:40:13 +0800

    bugfix

--- scripts/app/home/message/wait/homework/index.js
+++ scripts/app/home/message/wait/homework/index.js
@@ -1,3 +1,29 @@
+var D = require('drizzlejs');
 exports.items = {
     main: 'main'
+};
+
+exports.store = {
+    models: {
+        state: {},
+        works: {
+            url: '../course-study/course-front/audit-works',
+            type: 'pageable',
+            pageSize: 20,
+            root: 'items'
+        }
+    },
+    callbacks: {
+        init: function() {
+            return this.get(this.models.works);
+        },
+        refreshList: function(payload) {
+            D.assign(this.models.works.params, payload);
+            return this.get(this.models.works);
+        }
+    }
+};
+
+exports.beforeRender = function() {
+    return this.dispatch('init');
 };
