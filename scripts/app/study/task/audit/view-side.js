297dff3ad046919808cea57c3e2bed27d531db77
tangjie <tangjie@zhixueyun.com>
2017-06-03 17:57:01 +0800

    update side

--- scripts/app/study/task/audit/view-side.js
+++ scripts/app/study/task/audit/view-side.js
@@ -1,4 +1,35 @@
+var _ = require('lodash/collection');
 exports.bindings = {
     sectionStudyProgress: false,
-    task: true
+    task: true,
+    download: false
+};
+
+exports.dataForTemplate = {
+    task: function(data) {
+        var task = data.task,
+            me = this;
+        _.map(task.attachments || [], function(attach) {
+            var obj = attach;
+            if (obj.contentType && obj.contentType === 1) {
+                obj.preview = true;
+            }
+            obj.downUrl = me.bindings.download.getFullUrl() + '?id=' + obj.attachmentId;
+        });
+        return task;
+    },
+    sectionStudyProgress: function(data) {
+        var sectionStudyProgress = data.sectionStudyProgress,
+            me = this;
+        _.map(sectionStudyProgress.sectionAttachments || [], function(attach) {
+            var obj = attach;
+            if (obj.contentType && obj.contentType === 1) {
+                obj.preview = true;
+            }
+            if (obj.attachmentId) {
+                obj.downUrl = me.bindings.download.getFullUrl() + '?id=' + obj.attachmentId;
+            }
+        });
+        return sectionStudyProgress;
+    }
 };
