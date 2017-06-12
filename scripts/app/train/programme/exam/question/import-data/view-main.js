b80ba367de08e27768745d76708fbd0a7fd63e68
wangdongyan <wangdongyan@zhixueyun.com>
2017-06-12 18:59:13 +0800

    班级基本信息合影时间修改以及添加考试更新

--- scripts/app/train/programme/exam/question/import-data/view-main.js
+++ scripts/app/train/programme/exam/question/import-data/view-main.js
@@ -1,6 +1,12 @@
+var errors = require('./app/util/errors'),
+    _ = require('lodash/collection');
+
 exports.bindings = {
-    exportQuestionTemplate: true,
-    importData: true
+    exportQuestionTemplate: false,
+    exportDepotTemplate: false,
+    importResult: true,
+    state: false,
+    download: false
 };
 
 exports.events = {
@@ -21,18 +27,46 @@ exports.handlers = {
 };
 
 exports.dataForTemplate = {
-    downloadTemplateUrl: function() {
-        var model = this.bindings.exportQuestionTemplate,
+    downloadTemplateUrl: function(data) {
+        var model = data.state.templateType === 1
+                ? this.bindings.exportQuestionTemplate : this.bindings.exportDepotTemplate,
             url = model.getFullUrl() + '?',
             token = this.app.global.OAuth.token.access_token;
+        if (data.state.isOtherModuleType) {
+            url += 'isOtherModuleType=' + data.state.isOtherModuleType + '&';
+        }
+        if (data.state.organizationId) {
+            url += 'organizationId=' + data.state.organizationId + '&';
+        }
         url += ('exportType=1&access_token=' + token);
         return url;
     },
-    errorDataUrl: function() {
-        var model = this.bindings.exportQuestionTemplate,
-            url = model.getFullUrl() + '?',
-            token = this.app.global.OAuth.token.access_token;
-        url += ('exportType=3&access_token=' + token);
-        return url;
+    errorDataUrl: function(data) {
+        var model = this.bindings.download;
+        return model.getFullUrl() + '?id=' + data.importResult.errorFileId;
+    },
+    // importResult: function(data) {
+    //     return D.assign(data.importResult, {
+    //         errorList: _.map(data.importResult.errorList, function(e, i) {
+    //             return D.assign(e, {
+    //                 n: i + 1,
+    //                 errors: _.map(e.errors, function(ee) {
+    //                     return D.assign(ee, {
+    //                         code: errors.get(ee.code)
+    //                     });
+    //                 })
+    //             });
+    //         })
+    //     });
+    // },
+    errorMsg: function(data) {
+        if (data.importResult) {
+            return _.map(data.importResult.errorList, function(error) {
+                var msg = '不能为空';
+                if (error.code) msg = errors.get(error.code);
+                return '第' + error.row + '行, 第' + (error.column + 1) + '列有错误。[' + msg + ']';
+            });
+        }
+        return '';
     }
 };
