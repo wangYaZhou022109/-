a77ee65826e0923faa0a63c0a5a1ffb8e193a143
wangyazhou <wangyazhou@zhixueyun.com>
2017-06-12 09:13:24 +0800

    提交了问吧里的加载到最后没有更多了的提示

--- scripts/app/ask/myquiz/index.js
+++ scripts/app/ask/myquiz/index.js
@@ -18,7 +18,7 @@ exports.store = {
             data: [],
             params: { page: 1, size: 2 },
             mixin: {
-                findById: function(id) {
+                findById: function (id) {
                     var trends = this.module.store.models.page.data;
                     return _.find(trends, ['id', id]);
                 }
@@ -27,9 +27,9 @@ exports.store = {
         speech: {
             url: '../system/speech-set',
             mixin: {
-                getData: function(id) {
+                getData: function (id) {
                     var speechset;
-                    _.forEach(this.data, function(d) {
+                    _.forEach(this.data, function (d) {
                         if (d.id === id) {
                             speechset = d;
                         }
@@ -40,10 +40,10 @@ exports.store = {
         }
     },
     callbacks: {
-        refresh: function() {
+        refresh: function () {
             this.models.callback();
         },
-        set: function(payload) {
+        set: function (payload) {
             this.models.callback = payload;
         },
         // init: function() {
@@ -51,14 +51,14 @@ exports.store = {
         //     questions.set({ id: 1 });
         //     return this.get(questions);
         // },
-        init: function() {
+        init: function () {
             var questions = this.models.questions,
                 params = this.models.page.params,
                 page = this.models.page;
 
             params.id = 'null';
             questions.set(params);
-            return this.post(questions).then(function() {
+            return this.post(questions).then(function () {
                 page.data = questions.data;
                 page.changed();
             });
@@ -73,55 +73,55 @@ exports.store = {
         //         // me.models.page.params.page++;
         //     });
         // },
-        page: function() {
+        page: function () {
             var questions = this.models.questions;
             var params = this.models.page.params;
             var page = this.models.page;
             // var me = this;
             params.id = 'null';
             questions.set(params);
-            this.post(questions).then(function() {
+            this.post(questions).then(function () {
                 page.data.push.apply(page.data, questions.data);
                 page.changed();
             });
         },
-        remove: function(payload) {
+        remove: function (payload) {
             var questions = this.models.questions;
             questions.set(payload);
             return this.get(questions);
         },
-        follow: function(payload) {
+        follow: function (payload) {
             var follow = this.models.follow;
             follow.set(payload);
             return this.post(follow);
         },
-        unfollow: function(payload) {
+        unfollow: function (payload) {
             var follow = this.models.unfollow;
             // console.log(payload);
             follow.set(payload);
             return this.put(follow);
         },
-        shut: function(payload) {
+        shut: function (payload) {
             var shut = this.models.shut,
                 page = this.models.page,
                 me = this;
 
             shut.set(payload);
-            return this.chain(me.put(this.models.shut), function() {
-                page.data = _.filter(page.data, function(item) {
+            return this.chain(me.put(this.models.shut), function () {
+                page.data = _.filter(page.data, function (item) {
                     return item.id !== payload.id;
                 });
                 page.changed();
             });
         },
-        publish: function(payload) {
+        publish: function (payload) {
             var discuss = this.models.discuss,
                 data = payload,
                 speechset = this.models.speech.getData('2'),
                 me = this;
             data.speechset = speechset.status;
             discuss.set(data);
-            this.save(discuss).then(function() {
+            this.save(discuss).then(function () {
                 var message = '发布成功';
                 var page = me.models.page;
                 var speech = me.models.speech.getData('2');
@@ -132,22 +132,29 @@ exports.store = {
                 page.changed();
             });
         },
-        speech: function() {
+        speech: function () {
             var speech = this.models.speech;
             return this.get(speech);
         },
     }
 };
 
-exports.afterRender = function() {
+exports.afterRender = function () {
     var me = this;
     // console.log(this.renderOptions);
-    $(window).scroll(function() {
+    $(window).scroll(function () {
         var page = me.store.models.page.params.page;
         var size = me.store.models.page.params.size;
+        var scrollTop = $(document).scrollTop();
+        var clientHeight = $(window).height();
+        var scrollHeight = $(document).height();
+        var footerHeight = $('.footer').height();
         if (page * size === me.store.models.page.data.length) {
             me.store.models.page.params.page++;
             me.dispatch('page');
+        }
+        if ((scrollTop + clientHeight) >= (scrollHeight - footerHeight)) {
+            $('.none-more').css('display', 'block');
         }
     });
     this.dispatch('set', this.renderOptions.callback);
--- scripts/app/ask/myshares/index.js
+++ scripts/app/ask/myshares/index.js
@@ -16,13 +16,13 @@ exports.store = {
             data: [],
             params: { page: 1, size: 2 },
             mixin: {
-                findById: function(id) {
+                findById: function (id) {
                     var myshares = this.module.store.models.page.data;
                     return _.find(myshares, ['id', id]);
                 },
-                praise: function() {
+                praise: function () {
                     var data = [];
-                    _.forEach(this.data, function(d) {
+                    _.forEach(this.data, function (d) {
                         var obj = d;
                         if (d.question.praiseNum >= 0) {
                             obj.question.praiseNum = d.question.praiseNum + 1;
@@ -34,9 +34,9 @@ exports.store = {
                     });
                     return data;
                 },
-                unpraise: function() {
+                unpraise: function () {
                     var data = [];
-                    _.forEach(this.data, function(d) {
+                    _.forEach(this.data, function (d) {
                         var obj = d;
                         if (d.question.praiseNum > 0) {
                             obj.question.praiseNum = d.question.praiseNum - 1;
@@ -56,9 +56,9 @@ exports.store = {
         speech: {
             url: '../system/speech-set',
             mixin: {
-                getData: function(id) {
+                getData: function (id) {
                     var speechset;
-                    _.forEach(this.data, function(d) {
+                    _.forEach(this.data, function (d) {
                         if (d.id === id) {
                             speechset = d;
                         }
@@ -69,31 +69,31 @@ exports.store = {
         }
     },
     callbacks: {
-        refresh: function() {
+        refresh: function () {
             this.models.callback();
         },
-        set: function(payload) {
+        set: function (payload) {
             this.models.callback = payload;
         },
-        praise: function(payload) {
+        praise: function (payload) {
             var praise = this.models.praise,
                 me = this;
             praise.set(payload);
-            return this.post(praise).then(function() {
+            return this.post(praise).then(function () {
                 var page = me.models.page;
                 var curObj = page.findById(payload.id);
 
                 curObj.praise = true;
-                curObj.praiseNum ++;
+                curObj.praiseNum++;
 
                 page.changed();
             });
         },
-        unpraise: function(payload) {
+        unpraise: function (payload) {
             var unpraise = this.models.unpraise,
                 me = this;
             unpraise.set(payload);
-            return this.chain(me.put(this.models.unpraise), function() {
+            return this.chain(me.put(this.models.unpraise), function () {
                 var page = me.models.page;
                 var curObj = page.findById(payload.id);
 
@@ -103,61 +103,61 @@ exports.store = {
                 page.changed();
             });
         },
-        init: function() {
+        init: function () {
             var myshares = this.models.myshares,
                 params = this.models.page.params,
                 page = this.models.page;
 
             params.id = 'null';
             myshares.set(params);
-            return this.post(myshares).then(function() {
+            return this.post(myshares).then(function () {
                 page.data = myshares.data;
                 page.changed();
             });
         },
-        page: function() {
+        page: function () {
             var myshares = this.models.myshares;
             var params = this.models.page.params;
             var page = this.models.page;
             // var me = this;
             params.id = 'null';
             myshares.set(params);
-            this.post(myshares).then(function() {
+            this.post(myshares).then(function () {
                 page.data.push.apply(page.data, myshares.data);
                 page.changed();
             });
         },
-        follow: function(payload) {
+        follow: function (payload) {
             var follow = this.models.follow;
             follow.set(payload);
             return this.post(follow);
         },
-        unfollow: function(payload) {
+        unfollow: function (payload) {
             var unfollow = this.models.unfollow;
             unfollow.set(payload);
             return this.put(unfollow);
         },
-        shut: function(payload) {
+        shut: function (payload) {
             var shut = this.models.shut,
                 page = this.models.page,
                 me = this;
 
             shut.set(payload);
-            return this.chain(me.put(this.models.shut), function() {
-                page.data = _.filter(page.data, function(item) {
+            return this.chain(me.put(this.models.shut), function () {
+                page.data = _.filter(page.data, function (item) {
                     return item.id !== payload.id;
                 });
                 page.changed();
             });
         },
-        publish: function(payload) {
+        publish: function (payload) {
             var discuss = this.models.discuss,
                 data = payload,
                 speechset = this.models.speech.getData('2'),
                 me = this;
             data.speechset = speechset.status;
             discuss.set(data);
-            this.save(discuss).then(function() {
+            this.save(discuss).then(function () {
                 var message = '发布成功';
                 var page = me.models.page;
                 var speech = me.models.speech.getData('2');
@@ -168,21 +168,28 @@ exports.store = {
                 page.changed();
             });
         },
-        speech: function() {
+        speech: function () {
             var speech = this.models.speech;
             return this.get(speech);
         },
     }
 };
 
-exports.afterRender = function() {
+exports.afterRender = function () {
     var me = this;
-    $(window).scroll(function() {
+    $(window).scroll(function () {
         var page = me.store.models.page.params.page;
         var size = me.store.models.page.params.size;
+        var scrollTop = $(document).scrollTop();
+        var clientHeight = $(window).height();
+        var scrollHeight = $(document).height();
+        var footerHeight = $('.footer').height();
         if (page * size === me.store.models.page.data.length) {
             me.store.models.page.params.page++;
             me.dispatch('page');
+        }
+        if ((scrollTop + clientHeight) >= (scrollHeight - footerHeight)) {
+            $('.none-more').css('display', 'block');
         }
     });
     this.dispatch('set', this.renderOptions.callback);
