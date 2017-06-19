c7114a5ca571b835529418ef82eff86e541e515b
liushunan <liushunan@zhixueyun.com>
2017-06-19 14:41:19 +0800

    19901/19906

--- scripts/app/ask/myquiz/reply/view-content.js
+++ scripts/app/ask/myquiz/reply/view-content.js
@@ -8,6 +8,7 @@ exports.bindings = {
 
 exports.events = {
     'click answer-*': 'answer',
+    'click report-*': 'report',
 };
 
 exports.handlers = {
@@ -29,7 +30,18 @@ exports.handlers = {
             el.style.display = 'inline';
         }
         this.bindings.state.data.id = id;
-    }
+    },
+    report: function(payload) {
+        var id = payload,
+            data = { };
+        if (id.indexOf('_') !== -1) {
+            data = id.split('_');
+            this.app.viewport.modal(this.module.items['ask/report'], {
+                id: data[1],
+                objectType: data[0],
+                beUserId: data[2] });
+        }
+    },
 };
 
 exports.actions = {
