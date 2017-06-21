a8ac3a0405fc9755ff66055f56d7c2fa9cba1e90
张华 <zhanghua@zhixueyun.com>
2017-06-21 15:36:14 +0800

    Signed-off-by: 张华 <zhanghua@zhixueyun.com> 业务修正

--- scripts/app/ask/alldynamic/index.js
+++ scripts/app/ask/alldynamic/index.js
@@ -19,7 +19,7 @@ exports.store = {
         close: { url: '../ask-bar/question/close-status' },
         page: {
             data: [],
-            params: { page: 1, size: 10 },
+            params: { page: 1, size: 2 },
             mixin: {
                 closerefresh: function(id, type) {
                     var newData = [];
