2338bf28b339cc83c1dc06e1fc9db8f79bedfa01
tangjie <tangjie@zhixueyun.com>
2017-06-22 20:08:59 +0800

    bugfix

--- scripts/app/center/study/course/index.js
+++ scripts/app/center/study/course/index.js
@@ -38,6 +38,13 @@ exports.store = {
             this.get(progressList);
             searchModel.changed();
         },
+        enterSearch: function(params) {
+            var searchModel = this.models.search,
+                progressList = this.models.progressList;
+            D.assign(progressList.params, D.assign(searchModel.data, params));
+            this.get(progressList);
+            searchModel.changed();
+        },
         deleteProgress: function(params) {
             var progressModel = this.models.progress;
             progressModel.set(params);
