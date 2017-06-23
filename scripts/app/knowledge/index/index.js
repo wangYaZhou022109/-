64118dc0c9930e51f3d68dbdb6de4ef63103918d
yiwenbin <yiwenbin@zhixueyun.com>
2017-06-23 18:24:40 +0800

    20170623 第二次commit

--- scripts/app/knowledge/index/index.js
+++ scripts/app/knowledge/index/index.js
@@ -54,7 +54,6 @@ exports.store = {
                 categories = this.models.categories,
                 companyOrganization = this.app.global.currentUser.companyOrganization || {};
             search.set({ orderType: '0' }, true);
-            this.models.menu2.set(this.models.categories.findLevel(3), true);
             categories.params = {
                 companyId: companyOrganization.id,
                 rootOrganizationId: this.app.global.currentUser.rootOrganization.id
